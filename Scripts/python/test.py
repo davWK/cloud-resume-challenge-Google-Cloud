693957d692a068a9f74580cd84b9d4af804cefe0
davWK <davwoglo@gmail.com>
2022-08-17 21:06:26 +0000

    Refine test.py

--- Scripts/python/test.py
+++ Scripts/python/test.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 import json
-
-from main import getCount
 import requests
 import unittest
 
 
 class testgetCount(unittest.TestCase):
-    def do_test(self):
-        self.assertTrue(type(getCount()) is dict)
 
     def test_get_visitor_number_response_is_dict(self):
         response = requests.get("https://us-central1-alilikpo-228.cloudfunctions.net/get_visitor_number")
